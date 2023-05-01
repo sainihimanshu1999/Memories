477898f152737ebecd18a600831c1e1b4c19182e
sainihimanshu1999 <sainihimanshu.1999@gmail.com>
2023-05-01 20:13:44 +0530

    form creation done

--- server/index.js
+++ server/index.js
@@ -8,11 +8,13 @@ import postRoutes from './routes/post.js'
 
 const app = express()
 
-app.use('/posts', postRoutes)
+
 
 app.use(bodyParser.json({limit:'30mb', extended: true}))
 app.use(bodyParser.urlencoded({limit:'30mb', extended: true}))
 app.use(cors())
+
+app.use('/posts', postRoutes)
 
 
 const CONNECTION_URL = 'mongodb+srv://sainihimanshu1999:25december1999@cluster0.vhb34sa.mongodb.net/?retryWrites=true&w=majority'
